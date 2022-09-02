e460248c3d46c3da30b3b61eacc5fd9812dfc5d1
Diego Pérez Hernández <59278575+DiegopH-m@users.noreply.github.com>
2022-09-02 12:25:29 -0500

    Develop (#3)
    * Basic changes in main page
    
    * Correct warnings
    
    * Removed grid
    
    Co-authored-by: LuisRG30 <luisrg3012@gmail.com>

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -1,5 +1,3 @@
-
-
 import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
@@ -26,7 +24,7 @@ function generateRandom(min: number, max: number) {
 
   // generate random number 
   let rand = Math.random();
-
+  
   // multiply with difference 
   rand = Math.floor( rand * difference);
 
@@ -87,8 +85,9 @@ const posts = [post1];
 const sidebar = {
   title: 'About',
   description:
-    'We, as a startup company, believe that organizations, big and small, can benefit from data driven processes and decision making as a result of data being at the center. \
-     We strive to intelligently care, manage and display data to improve business culture and results.',
+
+    'We, as a startup company, believe that organizations, big and small, can benefit from data driven processes and decision making as a result of data being at the center. We strive to intelligently care, manage and display data to improve business culture and results.',
+
   archives: [
     { title: 'March 2020', url: '#' },
     { title: 'February 2020', url: '#' },
