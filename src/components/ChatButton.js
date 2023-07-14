c112e49fe740b39248142ab2ef9d0c5d58910bf6
Luis Ramirez Guzman <luisramirezguzman@Luiss-MacBook-Air.local>
2023-07-14 13:16:53 -0600

    Cock

--- src/components/ChatButton.js
+++ src/components/ChatButton.js
@@ -1,15 +1,21 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import './ChatButton.css';
+
+import useWebSocket from "react-use-websocket";
+
+const web_token = "arribaelatlas";
+const serviceURL = "anotherai.azurewebsites.net/"
 
 const ChatButton = () => {
   const [value, setValue] = useState("");
   const [ previousChats, setPreviousChats] = useState([]);
   const [isChatOpen, setChatOpen] = useState(false);
 
-  /*
   const [converesationName, setConversationName] = useState(
-    JSON.parse(localStorage.getItem("anonymousConversation") || '{"conversationName": ""}')
-  )*/
+    JSON.parse(localStorage.getItem("anonymous") || `{"conversationName": "${(Math.random() + 1).toString(36).substring(7)}"}`).conversationName
+  )
+
+  const ref = useRef(null);
 
   const handleButtonClick = () => {
     setChatOpen(!isChatOpen);
@@ -19,30 +25,54 @@ const ChatButton = () => {
     setChatOpen(false);
   };
 
+  
+
+  const { sendJsonMessage } = useWebSocket(
+    `wss://${serviceURL}anonymous/?web_token=${web_token}&conversation_name=${converesationName}`,
+    {
+      onOpen: () => console.log("opened"),
+      shouldReconnect: (closeEvent) => true,
+      onMessage: (event) => {
+        const data = JSON.parse(event.data);
+        switch (data.type) {
+          case "chat_message_echo":
+            setPreviousChats([...previousChats, data.message]);
+            ref.current.scrollIntoView({ behavior: "smooth" })
+            break;
+          case "last_messages":
+            conv = data.conversation_name
+            setConversationName(conv)
+            localStorage.setItem("anonymous", JSON.stringify({converesationName: conv}))
+            setPreviousChats([...data.messages.reverse()]);
+            ref.current.scrollIntoView({ behavior: "smooth" })
+            break;
+          default:
+            break;
+        }
+      },
+    }
+  );
+
   const handleSendMessage = () => {
     if (value.trim() !== '') {
-      const newMessage = {
-        role: 'user',
-        content: value,
-      };
-      setPreviousChats([...previousChats, newMessage]);
-      setValue('');
+      sendJsonMessage({
+        type: "chat_message",
+        message: value,
+      })
     }
+    setValue("");
   };
-
-    const currentChat = previousChats.filter(previousChat => previousChat.title === "Chat" );
 
   return (
     <div className={`chat-button ${isChatOpen ? 'open' : ''}`}>
       {isChatOpen ? (
         <div className="chat-window">
-          <div className="feed">
-            {currentChat.map((chatMessage, index) => (
-              <li key={index} className={`chat-message ${chatMessage.role === 'user' ? 'sent' : 'received'}`}>
-                <p>{chatMessage.content}</p>
+          <div className="feed" >
+            {previousChats.map((chatMessage, index) => (
+              <li key={index} className={`chat-message ${chatMessage.from_user.profile.Anonymous ? 'sent' : 'received'}`}>
+                <p>{chatMessage.message}</p>
               </li>
             ))}
-
           </div>
 
           <input
